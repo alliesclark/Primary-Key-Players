0b38d8357b8ea0218693926c6e13e1ea259dcdb9
sabrinavale <sabrinaelena.v@gmail.com>
2024-12-02 14:55:12 -0500

    header for wade

--- app/src/pages/Wade_Home_Page.py
+++ app/src/pages/Wade_Home_Page.py
@@ -10,3 +10,7 @@ st.set_page_config(layout = 'wide', page_title='Co-op Connect')
 
 SideBarLinks(show_home=True)
 
+with ui.element("div", className="flex flex-col border rounded-lg shadow p-4 m-2", key="view_jobs_card"):
+    ui.element("h2", children=["Welcome Wade!"], className="text-2xl font-bold text-gray-800", key="maura_welcome")
+    ui.element("div", children=["\n\n"], key="maura_divider")
+    ui.element("p", children=["As a student with previous co-op experience, this page will be your hub for key resources most useful to you."], className="text-gray-600")
