86944b6c8ba63e822555e214ce92897ab6a62a48
sabrinaval <sabrinaelena.v@gmail.com>
2024-12-01 17:48:40 -0800

    delete job position route

--- api/backend/job_positions/job_positions_routes.py
+++ api/backend/job_positions/job_positions_routes.py
@@ -303,124 +303,33 @@ def update_job_position():
     response.status_code = 200
     return response
     
-# # ------------------------------------------------------------
-# # Get the top 5 most expensive products from the database
-# @students.route('/mostExpensive')
-# def get_most_pop_products():
 
-#     query = '''
-#         SELECT product_code, 
-#                product_name, 
-#                list_price, 
-#                reorder_level
-#         FROM products
-#         ORDER BY list_price DESC
-#         LIMIT 5
-#     '''
-    
-#     # Same process as handler above
-#     cursor = db.get_db().cursor()
-#     cursor.execute(query)
-#     theData = cursor.fetchall()
- 
-#     response = make_response(jsonify(theData))
-#     response.status_code = 200
-#     return response
+#------------------------------------------------------------
+# Delete a specific job position
+@job_position.route('/job-position', methods=['DELETE'])
+def delete_job_position():
+    # Collecting data from the request object
+    job_position_data = request.json
+    current_app.logger.info(job_position_data)
 
-# # ------------------------------------------------------------
-# # Route to get the 10 most expensive items from the 
-# # database.
-# @students.route('/tenMostExpensive', methods=['GET'])
-# def get_10_most_expensive_products():
-    
-#     query = '''
-#         SELECT product_code, 
-#                product_name, 
-#                list_price, 
-#                reorder_level
-#         FROM products
-#         ORDER BY list_price DESC
-#         LIMIT 10
-#     '''
-    
-#     # Same process as above
-#     cursor = db.get_db().cursor()
-#     cursor.execute(query)
-#     theData = cursor.fetchall()
-    
-#     response = make_response(jsonify(theData))
-#     response.status_code = 200
-#     return response
-    
+    # Extracting the variables
+    job_id = job_position_data['id']
 
-# # ------------------------------------------------------------
-# # This is a POST route to add a new product.
-# # Remember, we are using POST routes to create new entries
-# # in the database. 
-# @students.route('/product', methods=['POST'])
-# def add_new_product():
-    
-#     # In a POST request, there is a 
-#     # collecting data from the request object 
-#     the_data = request.json
-#     current_app.logger.info(the_data)
+    # Constructing the query
+    query = f'''
+        DELETE FROM job_position
+        WHERE id = {job_id}
+    '''
+    current_app.logger.info(query)
 
-#     #extracting the variable
-#     name = the_data['product_name']
-#     description = the_data['product_description']
-#     price = the_data['product_price']
-#     category = the_data['product_category']
-    
-#     query = f'''
-#         INSERT INTO products (product_name,
-#                               description,
-#                               category, 
-#                               list_price)
-#         VALUES ('{name}', '{description}', '{category}', {str(price)})
-#     '''
-#     # TODO: Make sure the version of the query above works properly
-#     # Constructing the query
-#     # query = 'insert into products (product_name, description, category, list_price) values ("'
-#     # query += name + '", "'
-#     # query += description + '", "'
-#     # query += category + '", '
-#     # query += str(price) + ')'
-#     current_app.logger.info(query)
+    # Executing and committing the delete statement
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
 
-#     # executing and committing the insert statement 
-#     cursor = db.get_db().cursor()
-#     cursor.execute(query)
-#     db.get_db().commit()
-    
-#     response = make_response("Successfully added product")
-#     response.status_code = 200
-#     return response
+    response = make_response("Successfully deleted job position")
+    response.status_code = 200
+    return response
 
-# # ------------------------------------------------------------
-# ### Get all product categories
-# @students.route('/categories', methods = ['GET'])
-# def get_all_categories():
-#     query = '''
-#         SELECT DISTINCT category AS label, category as value
-#         FROM products
-#         WHERE category IS NOT NULL
-#         ORDER BY category
-#     '''
 
-#     cursor = db.get_db().cursor()
-#     cursor.execute(query)
-#     theData = cursor.fetchall()
-        
-#     response = make_response(jsonify(theData))
-#     response.status_code = 200
-#     return response
-
-# # ------------------------------------------------------------
-# # This is a stubbed route to update a product in the catalog
-# # The SQL query would be an UPDATE. 
-# @students.route('/product', methods = ['PUT'])
-# def update_product():
-#     product_info = request.json
-#     current_app.logger.info(product_info)
-
-#     return "Success"
+#------------------------------------------------------------
